3321876dec708f39ef83490addd90e53fed423ea
alex-Symbroson <alex.symbroson@gmail.com>
2022-12-08 00:35:43 +0100

    split enslaver

--- src/autorun.js
+++ src/autorun.js
@@ -1,9 +1,9 @@
 
+import * as enslave from "./enslave";
 import * as hack from "./hack";
 import { buy_upgrade } from "./nodehack";
 import * as srvd from "./serverData";
 import { BBServer, BBServerData } from "./servers";
-import { closeWeights, fn, fn2, selectWeighted } from "./util";
 import * as utilx from "./utilx";
 
 /** @type {NS}    */ var ns;
@@ -11,10 +11,7 @@ import * as utilx from "./utilx";
 /** @type {Partial<BBServerData>} */
 let data = {};
 
-const hackScript = { name: "mine.js", ram: 2.25 }
-const hackSlave = { name: "s_grow.js", ram: 1.75 }
 const stats = [[0], [0], [0]]
-let weighted = false;
 
 /** @param {NS} _ns */
 export async function main(_ns)
@@ -22,31 +19,21 @@ export async function main(_ns)
     data = srvd.init(ns = _ns);
     utilx.init(ns = _ns);
     hack.init(ns = _ns);
+    enslave.init(ns = _ns, stats);
     // flush port
     while (ns.readPort(1) != "NULL PORT DATA");
 
-    if (ns.args.includes('-w')) weighted = true;
-    const slaved = !ns.args.includes("-ns");
-    hackScript.ram = ns.getScriptRam(hackScript.name)
-    hackSlave.ram = ns.getScriptRam(hackSlave.name)
     var lastStats = "";
-
-    if (!slaved)
-    {
-        const svList = srvd.getServers(s => s.root)
-        for (const s of svList) mine(s, hackScript.ram,
-            (t, n) => ns.exec(hackScript.name, s.name, t, t, '--', n));
-    }
 
     for (var i = 0; ; i++)
     {
         const svList = srvd.getServers(s => s.root)
-        if (slaved && i % 2 == 0)
+        if (i % 2 == 0)
         {
             for (const s of svList) updateVals(ns, s)
-            for (const s of svList.filter(s => s.maxRam - ns.getServerUsedRam(s.name) > hackSlave.ram))
+            for (const s of svList.filter(s => s.maxRam - ns.getServerUsedRam(s.name) > enslave.hackSlave.ram))
             {
-                slave(s, svList);
+                enslave.slave(s, svList);
                 await ns.sleep(100);
             }
             while (handleMsg(String(ns.readPort(1))));
@@ -57,57 +44,13 @@ export async function main(_ns)
             lastStats = String(stats);
         }
 
-        if (i % 20 == 0)
-        {
-            srvd.scanServerPaths()
-            for (const s of srvd.getServers(s => !s.root && srvd.rootable(s)))
-            {
-                hack.hack(s);
-                hack.copy(s);
-                ns.tprint(`  EXEC home;connect ${data.servers[s.name].path.join(";connect ")};backdoor`)
-            }
-        }
+        if (i % 20 == 0) hackNewServers();
 
         buy_upgrade(ns)
         await ns.sleep(1000);
     }
 }
 
-/** @type {(s: BBServer, svList: BBServer[]) => void} */
-function slave(host, svList)
-{
-    const rootedServers = svList.filter(s => s.name != 'home')
-    const moneyServers = rootedServers.filter(s => s.maxMoney)
-    const weightedServers = !weighted ? moneyServers.map(e => ({ e, w: 1 })) :
-        closeWeights(moneyServers, s => s.maxRam, host.maxRam, 5)
-
-    mine(host, hackSlave.ram, (t, n) => enslave(host, weightedServers, t, n))
-}
-
-/** @type {(host: BBServer, ws: {e:BBServer, w:number}[], t: number, n: number) => number} */
-function enslave(host, ws, t, n)
-{
-    const moneyThreshFac = 0.9;
-    const secThreashFac = 1.5;
-
-    const target = selectWeighted(ws, s => s.w).e;
-    const moneyThresh = target.maxMoney * moneyThreshFac;
-    const secThresh = target.minSecLvl * secThreashFac;
-
-    ns.print(`money ${target.moneyAvail.toExponential(2)} / ${moneyThresh.toExponential(2)} / ${target.maxMoney.toExponential(2)})`)
-    ns.print(`sec   ${target.secLvl.toFixed(2)} / ${secThresh.toFixed(2)} / ${target.minSecLvl.toFixed(2)})`)
-    var pid = 0;
-
-    if (target.secLvl > secThresh)
-        pid = ns.exec("s_weaken.js", host.name, t, target.name, t, '--', n) << 2 | 0
-    else if (target.moneyAvail < moneyThresh)
-        pid = ns.exec("s_grow.js", host.name, t, target.name, t, '--', n) << 2 | 1
-    else
-        pid = ns.exec("s_hack.js", host.name, t, target.name, t, '--', n) << 2 | 2
-    
-    stats[pid % 4].push(pid >> 2);
-    return pid >> 2;
-}
 
 /** @type {(ns:NS, s:BBServer) => BBServer} */
 function updateVals(ns, s)
@@ -115,31 +58,6 @@ function updateVals(ns, s)
     s.moneyAvail = ns.getServerMoneyAvailable(s.name);
     s.secLvl = ns.getServerSecurityLevel(s.name);
     return s;
-}
-
-/** @param {BBServer} s */
-const getAvail = s => (s.maxRam / (s.name == 'home' ? 1.2 : 1) - ns.getServerUsedRam(s.name));
-
-/** @type {(s: BBServer, ram: number, exec: (threads: number, n: number) => number) => void} */
-function mine(s, ram, exec)
-{
-    var threads = Math.floor(getAvail(s) / ram);
-    const buf = s.name == "home" ? threads / 25 | 0 : 2 << 13
-    if (threads <= 0) return;
-
-    var n = threads / buf | 0;
-    // ns.tprint(`starting ${n}buf + ${threads - n * buf} on ${s.name} [${threads}]`);
-
-    const execerr = (x = 0) =>
-        utilx.err(`exec ${x} ${threads} ${s.name} ${threads * ram}/${fn(getAvail(s), 0, 2)}`)
-
-    if (!n)
-        exec(threads, 0) || execerr(2)
-    else while (n--)
-    {
-        exec(buf, 1 + Math.random() * 9998 | 0) || execerr(1)
-        threads -= buf;
-    }
 }
 
 /**
@@ -163,11 +81,21 @@ function handleMsg(s)
     return true;
 }
 
+function hackNewServers()
+{
+    srvd.scanServerPaths()
+    for (const s of srvd.getServers(s => !s.root && srvd.rootable(s)))
+    {
+        hack.hack(s);
+        hack.copy(s);
+        ns.tprint(`  EXEC home;connect ${data.servers[s.name].path.join(";connect ")};backdoor`)
+    }
+}
+
 /**
  * @param {BBServer} s
  */
 function registerMiner(s)
 {
     hack.copy(s);
-    mine(s, hackScript.ram, (t, n) => ns.exec(hackScript.name, s.name, t, t, '--', n));
 }
---
+++ src/enslave.js
@@ -0,0 +1,81 @@
+
+import { BBServer, BBServerData } from "./servers";
+import { closeWeights, fn, fn2, selectWeighted } from "./util";
+import * as utilx from "./utilx";
+
+/** @type {NS}    */ var ns;
+
+export const hackSlave = { name: "s_grow.js", ram: 1.75 }
+
+let weighted = false;
+let _stats = [[0]];   // ovrd from autorun
+
+/** @type {(ns:NS, stats:number[][]) => void} _ns */
+export function init(_ns, stats)
+{
+    utilx.init(ns = _ns);
+    _stats = stats;
+    if (ns.args.includes('-w')) weighted = true;
+    hackSlave.ram = ns.getScriptRam(hackSlave.name)
+}
+
+/** @type {(s: BBServer, svList: BBServer[]) => void} */
+export function slave(host, svList)
+{
+    const rootedServers = svList.filter(s => s.name != 'home')
+    const moneyServers = rootedServers.filter(s => s.maxMoney)
+    const weightedServers = !weighted ? moneyServers.map(e => ({ e, w: 1 })) :
+        closeWeights(moneyServers, s => s.maxRam, host.maxRam, 5)
+
+    mine(host, hackSlave.ram, (t, n) => enslave(host, weightedServers, t, n))
+}
+
+/** @type {(host: BBServer, ws: {e:BBServer, w:number}[], t: number, n: number) => number} */
+function enslave(host, ws, threads, n)
+{
+    const moneyThreshFac = 0.9;
+    const secThreashFac = 1.5;
+
+    const t = selectWeighted(ws, s => s.w).e;
+    const moneyThresh = t.maxMoney * moneyThreshFac;
+    const secThresh = t.minSecLvl * secThreashFac;
+
+    ns.print(`money ${t.moneyAvail.toExponential(2)} / ${moneyThresh.toExponential(2)} / ${t.maxMoney.toExponential(2)})`)
+    ns.print(`sec   ${t.secLvl.toFixed(2)} / ${secThresh.toFixed(2)} / ${t.minSecLvl.toFixed(2)})`)
+    var pid = 0;
+
+    if (t.secLvl > secThresh)
+        pid = ns.exec("s_weaken.js", host.name, threads, t.name, threads, '--', n) << 2 | 0
+    else if (t.moneyAvail < moneyThresh)
+        pid = ns.exec("s_grow.js", host.name, threads, t.name, threads, '--', n) << 2 | 1
+    else
+        pid = ns.exec("s_hack.js", host.name, threads, t.name, threads, '--', n) << 2 | 2
+    
+    _stats[pid % 4].push(pid >> 2);
+    return pid >> 2;
+}
+
+/** @param {BBServer} s */
+const getAvail = s => (s.maxRam / (s.name == 'home' ? 1.2 : 1) - ns.getServerUsedRam(s.name));
+
+/** @type {(s: BBServer, ram: number, exec: (threads: number, n: number) => number) => void} */
+function mine(s, ram, exec)
+{
+    var threads = Math.floor(getAvail(s) / ram);
+    const buf = s.name == "home" ? threads / 25 | 0 : 2 << 13
+    if (threads <= 0) return;
+
+    var n = threads / buf | 0;
+    // ns.tprint(`starting ${n}buf + ${threads - n * buf} on ${s.name} [${threads}]`);
+
+    const execerr = (x = 0) =>
+        utilx.err(`exec ${x} ${threads} ${s.name} ${threads * ram}/${fn(getAvail(s), 0, 2)}`)
+
+    if (!n)
+        exec(threads, 0) || execerr(2)
+    else while (n--)
+    {
+        exec(buf, 1 + Math.random() * 9998 | 0) || execerr(1)
+        threads -= buf;
+    }
+}
